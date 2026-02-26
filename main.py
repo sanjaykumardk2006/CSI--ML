b2710d7ab17949e2b9c104f241e56dc4514335cb
sanjaykumardk2006 <sanjaykumardk2006@gmail.com>
2026-02-26 16:17:36 +0530

    Add Flask app with prediction endpoint and model loading

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+import pickle
+import numpy as np
+import joblib
+from sklearn.datasets import load_iris
 
+targets = load_iris().target_names
 app=Flask(__name__)
+
+model =joblib.load('/model.joblib')
+# with open('/model.pkl', 'rb') as f:
+#     model = pickle.load(f)
 
 @app.route('/')
 def home():
-    return 'Home Page'
+    return 'Welcome to ML Model Flask App!'
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=request.get_json(force=True)
+    input_data=np.array(data['features']).reshape(1,-1)
+    prediction=model.predict(input_data)
+    output = targets[int(prediction[0])]
+    return jsonify({'prediction': output})
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=7860,debug=True)
